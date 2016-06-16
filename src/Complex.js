c51f618fc88dac9a7a3574b0c557a1f223c81bc8
jimmcgaw <jpmcgaw@gmail.com>
2016-06-15 21:01:07 -0700

    initial commit; complex number class with basic operations

---
+++ src/Complex.js
@@ -0,0 +1,57 @@
+export default class Complex {
+  constructor(real, imaginary){
+    this.real = real;
+    this.imaginary = imaginary;
+  }
+
+  equals(otherComplex){
+    if (!(otherComplex instanceof Complex)){
+      throw new TypeError("Value provided must also be of type 'Complex'.")
+    }
+    if (otherComplex.real === this.real && otherComplex.imaginary == this.imaginary){
+      return true;
+    }
+    return false;
+  }
+
+  add(otherComplex){
+    if (!(otherComplex instanceof Complex)){
+      throw new TypeError("Value provided must also be of type 'Complex'.")
+    }
+    return new Complex(otherComplex.real + this.real, otherComplex.imaginary + this.imaginary);
+  }
+
+  multiply(otherComplex){
+    if (!(otherComplex instanceof Complex)){
+      throw new TypeError("Value provided must also be of type 'Complex'.")
+    }
+    var f = this.real * otherComplex.real;
+    var o = this.real * otherComplex.imaginary;
+    var i = this.imaginary * otherComplex.real;
+    var l = this.imaginary * otherComplex.imaginary;
+    l = -l;
+    return new Complex(f + l, o + i);
+  }
+
+  exp(n){
+    if (n === 0){
+      return new Complex(this.real + 1, 0);
+    } else if (n === 1){
+      return this;
+    } else {
+      var c = this;
+      for (var i = 1; i < n; i++){
+        c = c.multiply(c);
+      }
+      return c;
+    }
+  }
+
+  complexConjugate(){
+    return new Complex(this.real, -this.imaginary);
+  }
+
+  prettyPrint(){
+    return this.real.toString() + this.imaginary.toString() + 'i';
+  }
+}
---
+++ test/Complex.test.js
@@ -0,0 +1,78 @@
+const expect = require('chai').expect;
+import Complex from '../src/complex';
+
+describe('Complex', function() {
+  describe('#constructor', function() {
+    it('sets real and imaginary correctly', () => {
+      var complex = new Complex(6, 7);
+      expect(complex.real).to.equal(6);
+      expect(complex.imaginary).to.equal(7);
+    });
+  });
+
+  describe('#equals', function() {
+    describe('given two complex numbers with the same values', function() {
+      it('shows them to be equal', function () {
+        var complex1 = new Complex(2,3);
+        var complex2 = new Complex(2,3);
+        expect(complex1.equals(complex2)).to.equal(true);
+      });
+    });
+
+    describe('given two complex numbers with different values', function() {
+      it('fails the equality test', function () {
+        var complex1 = new Complex(3, 4);
+        var complex2 = new Complex(66, 77);
+        expect(complex1.equals(complex2)).to.equal(false);
+      });
+    });
+  });
+
+  describe('#add', function() {
+    it('returns the sum of two Complex instances', function () {
+      var c1 = new Complex(4, 5);
+      var c2 = new Complex(3, -2);
+      var sum = c1.add(c2);
+      expect(sum.real).to.equal(7);
+      expect(sum.imaginary).to.equal(3);
+    });
+  });
+
+  describe('#multiply', function() {
+    it('returns the product of two Complex instances', function () {
+      var c1 = new Complex(4, 5);
+      var c2 = new Complex(3, 2);
+      var product = c1.multiply(c2);
+      expect(product.real).to.equal(2);
+      expect(product.imaginary).to.equal(23);
+    });
+  });
+
+  describe('#exp', function() {
+    describe('given a complex number raised to 2 power', function() {
+      it('returns the square of the complex number', function () {
+        var complex = new Complex(3, 5);
+        var squared = complex.exp(2);
+        var product = complex.multiply(complex);
+        expect(squared.real).to.equal(product.real);
+        expect(squared.imaginary).to.equal(product.imaginary);
+      });
+    });
+  });
+
+  describe('#complexConjugate', function() {
+    it('returns a new Complex instance which is the complex conjugate', function () {
+      var complex = new Complex(3, 4);
+      var conjugate = complex.complexConjugate();
+      expect(conjugate.real).to.equal(3);
+      expect(conjugate.imaginary).to.equal(-4);
+    });
+  });
+
+  describe('#prettyPrint', function() {
+    it('prints a string version of the complex number', function () {
+      var complex = new Complex(-3, -4);
+      expect(complex.prettyPrint()).to.equal('-3-4i');
+    });
+  });
+});
